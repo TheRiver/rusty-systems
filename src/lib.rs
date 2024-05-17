90cacafb8c148155fad72c028a683e0b1d035d72
Constance Neeser <TheRiver@users.noreply.github.com>
2024-05-17 11:23:20 +0200

    Added documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
 //! A crate for procedurally generating content using L-systems.
+//! 
+//! # Introduction
+//! 
+//! The supported [L-Systems][wiki] are _context-free_ and _stochastic_.  
+//! 
+//! The code repository is available on [GitHub](https://github.com/TheRiver/rusty-grammar/) and
+//! is distributed under an [MIT license](https://github.com/TheRiver/rusty-grammar/blob/main/LICENSE).
 //!
 //! # Parsing
 //!
@@ -18,9 +25,15 @@
 //! * [`system::parser::parse_production`] to parse individual productions.
 //! 
 //! See [`system::parser`] for more information.
+//! 
 //!
-//! The code repository is available on [GitHub](https://github.com/TheRiver/rusty-grammar/) and
-//! is distributed under an [MIT license](https://github.com/TheRiver/rusty-grammar/blob/main/LICENSE).
+//! # Learn more
+//!
+//! If you would like to learn more about L-Systems, the original *Algorithmic Beauty of Plants*
+//! book, by Prusinkiewicz and Lindenmayer, is [available for free, online][abop].
+//! 
+//! [wiki]: https://en.wikipedia.org/wiki/L-system
+//! [abop]: http://algorithmicbotany.org/papers/#abop
 
 pub mod error;
 pub mod tokens;
