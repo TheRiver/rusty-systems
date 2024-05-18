b9b81646c4dfe15f88e8ef69dc3c453871da266c
Constance Neeser <TheRiver@users.noreply.github.com>
2024-05-18 09:10:50 +0200

    Documentation updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 //! A crate for procedurally generating content using L-systems.
-//! 
+//!
+//! **NOTE:** this crate is still under early development, and might change rapidly.
+//!
 //! # Introduction
-//! 
-//! The supported [L-Systems][wiki] are _context-free_ and _stochastic_.  
+//!
+//! The supported [L-Systems][wiki] are _context-free_ and _stochastic_.
 //!
 //! # Parsing
 //!
@@ -22,32 +24,32 @@
 //! * [`system::parser::parse_production`] to parse individual productions.
 //! 
 //! See [`system::parser`] for more information.
-//! 
+//!
 //! # Examples
-//! 
+//!
 //! The crate's example directory has various examples:
-//! 
+//!
 //! * [Vector graphics plant][skia-plant]
-//! 
+//!
 //!   This example uses two rules to produce a small plant. The tokens
 //!   are interpreted using a classic [logo turtle][logo-turtle] interpretation
 //!   to produce vector graphics. While the example uses [tiny skia][tiny-skia],
 //!   this can be replaced with any vector graphic library.
-//! 
+//!
 //!   If you clone the code repository, you can run this using:
-//! 
+//!
 //!   ```cargo run --example skia-plant ```
 //!
 //! # Learn more
 //!
 //! If you would like to learn more about L-Systems, the original *Algorithmic Beauty of Plants*
 //! book, by Prusinkiewicz and Lindenmayer, is [available for free, online][abop].
-//! 
+//!
 //! # License
-//! 
+//!
 //! The code repository is available on [GitHub](https://github.com/TheRiver/rusty-grammar/) and
 //! is distributed under an [MIT license](https://github.com/TheRiver/rusty-grammar/blob/main/LICENSE).
-//! 
+//!
 //! [wiki]: https://en.wikipedia.org/wiki/L-system
 //! [abop]: http://algorithmicbotany.org/papers/#abop
 //! [skia-plant]: https://github.com/TheRiver/rusty-grammar/blob/main/examples/skia-plant/main.rs
